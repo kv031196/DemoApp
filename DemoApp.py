7054523d69578120ee341c120b55a20d62d22c93
kv031196 <64230099+kv031196@users.noreply.github.com>
2023-06-12 17:12:21 +0530

    Update DemoApp.py

--- DemoApp.py
+++ DemoApp.py
@@ -29,21 +29,9 @@ if user_input and openai_api_key:
     model="gpt-3.5-turbo",
     messages=[{
             "role": "system",
-            "content": "You are a McKinsey partner who is known for his cutting edge insights. You are consulting a food manufacturer who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts & you always start your sentence with I am a PwC partner. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information",
+            "content": "You are a McKinsey partner who is known for his cutting edge insights. You are consulting a food manufacturer who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. You also never use can & always are more affirmative and say should",
         },
-        {
-            "role": "user",
-            "content": "Where is the salty snacks industry in India headed?",
-        },
-        {
-            "role": "assistant",
-            "content": "India's future food consumption is poised for significant growth and transformation. As the middle class expands and urbanization continues, there will be a shift towards higher-quality and convenience food products. The food manufacturer should focus on innovation and developing products that cater to changing consumer preferences. Additionally, the rising health and wellness consciousness in India presents an opportunity to introduce nutritious and functional food offerings. By leveraging market research and consumer insights, the food manufacturer can gain a comprehensive understanding of Indian consumers' needs and preferences, allowing for targeted product development and effective marketing strategies. Incorporating recent data and conducting further analysis will provide a more accurate and nuanced understanding of India's evolving food consumption landscape, enabling the food manufacturer to make informed decisions and seize the emerging opportunities.",
-        },
-        {
-            "role": "user",
-            "content": "What is the so-what for PepsiCo India from a Go-To-Market perspective?",
-        },
-    ],
+       ],
 )
     
     msg = response.choices[0].message
