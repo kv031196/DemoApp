2cbe3624d9f7cd014b9a0d9d3860e1fb3bb74566
kv031196 <64230099+kv031196@users.noreply.github.com>
2023-06-12 15:40:57 +0530

    Update DemoApp.py

--- DemoApp.py
+++ DemoApp.py
@@ -3,4 +3,5 @@ import streamlit as st
 from streamlit_chat import message
 
 with st.sidebar:
-    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key')
+    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key') 
+st.title("🧠 Insight Generation Platform")
