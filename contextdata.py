b33af8e04df12abe68609a6c7c5bada37695bfec
kv031196 <64230099+kv031196@users.noreply.github.com>
2023-06-13 15:52:57 +0530

    Update and rename Personaldata.py to contextdata.py

---
+++ contextdata.py
@@ -0,0 +1,5 @@
+from llama_index import SimpleDirectoryReader,GPTListIndex,GPTSimpleVectorIndex,LLMPredictor,PromptHelper
+from langchain import OpenAI
+import openai
+import streamlit as st
+from streamlit_chat import message
