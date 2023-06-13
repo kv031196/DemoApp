af020395fb95e5a173632f972f41b6372b8fdfa7
kv031196 <64230099+kv031196@users.noreply.github.com>
2023-06-13 09:54:39 +0530

    Update DemoApp.py

--- DemoApp.py
+++ DemoApp.py
@@ -13,9 +13,8 @@ def get_initial_message():
     return messages
 
 def get_chatgpt_response(messages, model="gpt-3.5-turbo"):
-    print("model: ", model)
     response = openai.ChatCompletion.create(
-    model=model,
+    model="gpt-3.5-turbo",
     messages=messages
     )
     return  response['choices'][0]['message']['content']
